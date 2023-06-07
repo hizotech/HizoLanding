735064032fa1d04c62c9e5d738ae4c914fca0262
Sylvester Nkeze <snkeze9@gmail.com>
2023-06-07 19:55:35 +0100

    Update: Spacing and sizing updates made

--- components/Modals/MailConfirmation.vue
+++ components/Modals/MailConfirmation.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="py-10">
-    <img src="/img/success.svg" class="block w-32 mb-6 mx-auto" alt="" />
+    <!-- <img src="/img/success.svg" class="block w-32 mx-auto" alt="" /> -->
+    <div class="mb-6 text-[#3cdf90] flex justify-center">
+      <icon name="line-md:confirm-circle-twotone" size="10rem" />
+    </div>
     <p class="text-lg text-center max-w-md mx-auto">
       We’re happy to have you added <br />
       and
